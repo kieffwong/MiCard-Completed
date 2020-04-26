d3c7bc8aca5dca327af4ef00445a1155fbf0a97c
Kieff <44449801+kieffwong@users.noreply.github.com>
2020-04-26 16:45:15 +0100

    main.dart, fonts, images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,72 +12,175 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
+          child: Center(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/angela.jpg'),
-            ),
-            Text(
-              'Angela Yu',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 150.0,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('images/szeyu.jpg'),
+                ),
+                Text(
+                  'Bella Wong',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
                   ),
-                  title: Text(
-                    '+44 123 456 789',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                ),
+                Text(
+                  'Pokemon Trainer',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                    height: 10.0,
+                    width: 150.0,
+                    child: Divider(color: Colors.teal.shade200, height: 10.0)),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Strength:  ',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                      ],
                     ),
                   ),
-                )),
-            Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Weakness:',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.black12),
+                        Icon(Icons.star, color: Colors.black12),
+                        Icon(Icons.star, color: Colors.black12),
+                      ],
+                    ),
                   ),
-                  title: Text(
-                    'angela@email.com',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro'),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('images/eevee.jpg'),
+                ),
+                Text(
+                  'Eevee',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
                   ),
-                ))
-          ],
-        )),
+                ),
+                Text(
+                  'Pokemon',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 20.0,
+                    letterSpacing: 2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                    height: 10.0,
+                    width: 150.0,
+                    child: Divider(color: Colors.teal.shade200, height: 10.0)),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Power:   ',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Energy:  ',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                        Icon(Icons.star, color: Colors.yellow[500]),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
